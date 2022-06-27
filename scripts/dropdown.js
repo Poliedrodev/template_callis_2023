cb78e0d5dc11be8b488a203fdf58042e4e05b243
Luiza <lluizatrindade@gmail.com>
2022-06-27 17:02:35 -0300

    boxe e outros ajustes

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -43,5 +43,26 @@ $(document).ready(function () {
             }
         });
         //Fim do script para o componente
+
+        var containerCollapsibleConheca = $('.container_collapsible_conheca');
+        containerCollapsibleConheca.on('click', function () {
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+
+            if ($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+
+            if ($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
     });
 });
