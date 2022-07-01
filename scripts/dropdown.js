114228c9b3b69f2eb9d4390ad751b6df72249837
Luiza <lluizatrindade@gmail.com>
2022-06-30 21:00:07 -0300

    collapsible unificado

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -44,6 +44,28 @@ $(document).ready(function () {
         });
         //Fim do script para o componente
 
+        //Inicio do script para o componente
+        var containerVoceE = $('.container_collapsible_unificado');
+        containerVoceE.on('click', function () {
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+
+            if ($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+
+            if ($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+
         var containerCollapsibleConheca = $('.container_collapsible_conheca');
         containerCollapsibleConheca.on('click', function () {
             console.log('02');
